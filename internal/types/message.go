59be86ecf5bc07891bed32686df6f7919683bc01
Win Paing Oo <winpaing000@gmail.com>
2026-07-26 23:22:39 +0800

    Add message struct

--- internal/types/message.go
+++ internal/types/message.go
@@ -3,7 +3,7 @@ package types
 import "encoding/json"
 
 type Message struct {
-	ID      string
-	Channel string
-	Data    json.RawMessage
+	ID      string          `json:"id,omitempty"` //name in the json file
+	Channel string          `json:"channel"`
+	Data    json.RawMessage `json:"data"`
 }
