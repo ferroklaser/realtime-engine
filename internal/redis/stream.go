e0ca29ef9ed50473c5d08fda004a8238cd86b594
Win Paing Oo <winpaing000@gmail.com>
2026-07-28 17:14:52 +0800

    Add stream helper

---
+++ internal/redis/stream.go
@@ -0,0 +1,35 @@
+package redis
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"realtime/internal/types"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func SaveToStream(ctx context.Context, rdb *redis.Client, stream string, msg types.Message) error {
+	jsonData, err := json.Marshal(msg)
+
+	if err != nil {
+		log.Println("Error marshalling for stream")
+		return err
+	}
+
+	_, err2 := rdb.XAdd(
+		ctx,
+		&redis.XAddArgs{
+			Stream: stream,
+			Values: map[string]any{
+				"payload": string(jsonData),
+			},
+		}).Result()
+
+	if err2 != nil {
+		log.Println("Error adding to stream")
+		return err2
+	}
+
+	return nil
+}
